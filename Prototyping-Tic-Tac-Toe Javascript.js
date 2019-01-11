dd6d156f88cd0297e4fa8fb9a4a1a2cfaee131db
Malrayess <m.alrayess@share.epsb.ca>
2019-01-10 20:33:11 -0700

    Update

--- Prototyping-Tic-Tac-Toe Javascript.js
+++ Prototyping-Tic-Tac-Toe Javascript.js
@@ -1,12 +1,46 @@
-var canvas = document.getElementById("myCanvas");
+
+var canvas = document.getElementById("Canvas1");
 var ctx = canvas.getContext("2d"); //ctx = context
-var canvaswidth = canvas.width;
-var canvasheight = canvas.height;
+var cwidth = 200;
+var cheight = 100;
 
 ctx.fillStyle = "black";
-ctx.fillRect(0, 0, 400, 400);
+ctx.fillRect(0, 0, 200, 100);
+ctx.stroke();
 
-moveTo(canvaswidth*1/3, 0);
-ctx.lineTo(canvaswidth*1/3, canvasheight);
-ctx.strokeStyle = "#FFFFFF";
+ctx.fillStyle = "#FFFFFF";
+ctx.font = "20px Arial";
+ctx.fillText("Scoreboard", 0, 20); // text, x, y
+ctx.fillText("X: ", 0, 50);
+ctx.fillText("O: ", 100, 50);
+
+var canvas = document.getElementById("Canvas2");
+var ctx = canvas.getContext("2d"); //ctx = context
+var cwidth = 500;
+var cheight = 500;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 500, 500);
+ctx.stroke();
+
+// VERTICAL LINE 1
+ctx.strokeStyle = 'white';
+ctx.moveTo(cwidth/3, 0);
+ctx.lineTo(cwidth/3, cheight);
+ctx.lineWidth = 7;
+ctx.stroke();
+
+// VERTICAL LINE 2
+ctx.moveTo(cwidth*2/3, 0);
+ctx.lineTo(cwidth*2/3, cheight);
+ctx.stroke();
+
+// HORIZONTAL LINE 1
+ctx.moveTo(0, cheight/3);
+ctx.lineTo(cwidth, cheight/3);
+ctx.stroke();
+
+// HORIZONTAL LINE 2
+ctx.moveTo(0, cheight*2/3);
+ctx.lineTo(cwidth, cheight*2/3);
 ctx.stroke();
