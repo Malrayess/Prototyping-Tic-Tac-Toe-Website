e414796650259d917c757b4460669c7458100074
Malrayess <m.alrayess@share.epsb.ca>
2019-01-26 22:50:00 -0700

    Update

--- Prototyping-Tic-Tac-Toe Javascript.js
+++ Prototyping-Tic-Tac-Toe Javascript.js
@@ -1,5 +1,9 @@
-startup();
-function startup() {
+//GLOBAL
+let gameStop = false;
+let lastPressed = "";
+var x, y;
+var count = 0;
+
   var canvas = document.getElementById("Canvas1");
   var ctx = canvas.getContext("2d"); //ctx = context
   var cwidth = 200;
@@ -19,6 +23,7 @@ function startup() {
   var ctx = canvas2.getContext("2d"); //ctx = context
   var cwidth2 = 500;
   var cheight2 = 500;
+  var cellD = cwidth2/3;
 
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, 500, 500);
@@ -45,7 +50,9 @@ function startup() {
   ctx.moveTo(0, cheight2*2/3);
   ctx.lineTo(cwidth2, cheight2*2/3);
   ctx.stroke();
-}
+
+  lastPressed = "O";
+
 var row1 = ["","",""]; // creates array of first row
 var row2 = ["","",""]; // creates array of second row
 var row3 = ["","",""]; // creates array of third row
@@ -66,37 +73,102 @@ function initializeArray() { // function that initializes array
   }
 }
 
-/*document.onmousemove = function(evt) {
+document.onmousemove = function(evt) {
   //call function here
   mousePos = getMousePosition(evt);
-  console.log(x + "," + y);
 }
 function getMousePosition(evt) {
   var rect = Canvas2.getBoundingClientRect();
-  return {
-    x: Math.round(evt.clientX - rect.left),
-    y: Math.round(evt.clientY - rect.top)
-  };
-}*/
+    x = Math.round(evt.clientX - rect.left);
+    y = Math.round(evt.clientY - rect.top);
+}
 
 document.onmousedown = function() {
   //call function here
-  allPlacingCode();
+  allPlacingCode(x, y);
 }
 
-function allPlacingCode() {
+function allPlacingCode(x, y) {
     var c, r;
-    var cwidth2 = 500;
-    var cheight2 = 500;
 
-    c = 0 + (cwidth2)/3;
-    r = 0 + (cheight2)/3;
+    c = Math.floor(x/cellD);
+    r = Math.floor(y/cellD);
 
-    if (c>= cwidth2*0 && c<=cwidth2 && r>= cheight2*0 && r<=cheight2 && grid[r] == "") {
-      console.log("hi");
+    console.log(x, y, cellD);
+    console.log(c, r);
+    var row = grid[r]; // getting the row of the grid
+    if (c>=0 && c<=2 && r>=0 && r<=2 && row[c] == "" && gameStop == false) {
+        if (lastPressed == "X") {
+          placing(c, r, "O");
+          lastPressed = "O";
+          row[c] = "O"; // getting the value of the cell, column
+    } else {
+      placing(c, r, "X");
+      lastPressed = "X";
+      row[c] = "X";
     }
+  }
 }
 
-function placing() {
+function placing(c, r, symbol) {
+  var ctx = canvas2.getContext("2d"); //ctx = context
+  ctx.fillStyle = "#FFFFFF";
+  ctx.font = "120px Arial";
+  ctx.fillText(symbol, c*cellD + cellD/4, (r+1)*cellD - cellD/4);
+  ThreeInRow();
+}
 
+function ThreeInRow() {
+  let check3InRowX = false;
+  let check3InRowO = false;
+  let winX = false;
+  let winO = false;
+  var xWin = 0;
+  var oWin = 0;
+
+  check3inRow();
+}
+
+function check3inRow() {
+  if (checkRows("X")/* || checkColumn("X") || checkDiagonal("X")*/) {
+    gameStop = true;
+    xWin += 1;
+    printWinScore("X");
+  } else if (checkRows("O")/* || checkColumn("O") || checkDiagonal("O")*/) {
+    gameStop = true;
+    oWin += 1;
+    printWinScore("O");
+  } else if (count == 9) {
+    gameStop = true;
+  }
+}
+function printWinScore(winner) {
+  if (winner == "X") {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "20px Arial";
+    ctx.fillText("X: " + xWin, 0, 50);
+    ctx.fillText("O: " + oWin, 100, 50);
+  } else if (winner == "O") {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "20px Arial";
+    ctx.fillText("X: " + xWin, 0, 50);
+    ctx.fillText("O: " + oWin, 100, 50);
+  }
+}
+
+  function checkRows(xORo) {
+    var win = true;
+    for (let r = 0;r < 3; r++){
+      var row = grid[r]; // row = grid[r], row = whichever row specified in brackets (1, 2, 3)
+      win = true;
+      for (let c = 0; c < 3; c++){
+console.log(row[c]);
+        if (row[c] != xORo) {
+          win = false;
+          return win;
+        }
+      }
+    }
+    console.log("X win");
+    return win;
 }
